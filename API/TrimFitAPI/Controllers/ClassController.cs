09a9886533a7eea7e3b7ddfca2aab7a8922b59c8
Mikołaj Ferfet <m.ferfet@ponowemu.pl>
2018-11-22 21:01:40 +0100

    B

---
+++ API/TrimFitAPI/Controllers/ClassController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using TrimFitAPI.Models;
+
+namespace TrimFitAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClassController : ControllerBase
+    {
+        private readonly ClassContext _context;
+
+        public ClassController(ClassContext context)
+        {
+            _context = context;
+
+            if (_context.Classes.Count() == 0)
+            {
+                // Create a new TodoItem if collection is empty,
+                // which means you can't delete all TodoItems.
+                _context.Classes.Add(new Class { Class_description ="Opis", Class_id = 1, Class_name = "Tytuł", Class_active = true });
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult<List<Class>> GetAll()
+        {
+            return _context.Classes.ToList();
+        }
+
+        [HttpGet("{id}", Name = "GetTodo")]
+        public ActionResult<Class> GetById(long id)
+        {
+            var item = _context.Classes.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return item;
+        }
+
+    }
+
+
+}
---
+++ API/TrimFitAPI/Models/ClassContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrimFitAPI.Models;
+
+namespace TrimFitAPI.Models
+{
+    public class ClassContext : DbContext
+    {
+        public ClassContext(DbContextOptions<ClassContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Class> Classes { get; set; }
+    }
+}
