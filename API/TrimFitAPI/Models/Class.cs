d9445196f26b91a2b06af2d1c8cb25785ab2f69b
Mikołaj Ferfet <m.ferfet@ponowemu.pl>
2018-11-22 20:49:56 +0100

    A

---
+++ API/TrimFitAPI/Models/Class.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrimFitAPI.Models
+{
+    public class Class
+    {
+        public int Class_id { get; set; }
+        public string Class_name { get; set; }
+        public string Class_description { get; set; }
+        public bool Class_active { get; set; }
+    }
+}
