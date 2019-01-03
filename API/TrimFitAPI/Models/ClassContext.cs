51fd6f235d8217508f8f5f89a435cdd878ceb824
Mikołaj Ferfet <m.ferfet@ponowemu.pl>
2019-01-03 21:35:11 +0100

    New database

--- API/TrimFitAPI/Models/ClassContext.cs
+++ API/TrimFitAPI/Models/ClassContext.cs
@@ -9,9 +9,24 @@ namespace TrimFitAPI.Models
             : base(options)
         {
         }
-
-        public DbSet<Class> Classes { get; set; }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public DbSet<Activity> Activity { get; set; }
+        public DbSet<Activity_club> {get; set;}
+        public DbSet<Club> {get; set;}
+        public DbSet<Customer> {get; set;}
+        public DbSet<Customer_club> {get; set;}
+        public DbSet<Employee> {get; set;}
+        public DbSet<Employee_club> {get; set;}
+        public DbSet<Payment> {get; set;}
+        public DbSet<Payment_type> {get; set;}
+        public DbSet<Position> {get; set;}
+        public DbSet<Position_club> {get; set;}
+        public DbSet<Registration> {get; set;}
+        public DbSet<Room> {get; set;}
+        public DbSet<Room_club> {get; set;}
+        public DbSet<Timetable> {get; set;}
+        public DbSet<Voucher> {get; set;}
+        public DbSet<Voucher_type> {get; set;}
+protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseNpgsql("Host=23202.p.tld.pl;Database=pg23202_trimfit;Username=pg23202_trimfit;Password=Bazatf8!");
         }
