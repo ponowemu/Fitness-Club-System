c3cf3fc341c107ce7a8407789cb166714016a30b
Michał Kamiński <michal.mjk1@gmail.com>
2019-03-19 22:46:47 +0100

    Initial mobile UI
    Layout, strings, login screen

---
+++ MOBILE/TrimfitMobile/app/src/main/java/com/smartica/trimfitmobile/ui/login/LoggedInUserView.kt
@@ -0,0 +1,9 @@
+package com.smartica.trimfitmobile.ui.login
+
+/**
+ * User details post authentication that is exposed to the UI
+ */
+data class LoggedInUserView(
+    val displayName: String
+    //... other data fields that may be accessible to the UI
+)
