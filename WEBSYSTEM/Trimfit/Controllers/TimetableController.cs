55f4ce0a74c84059e40d58b8330148072b5718f5
Mikołaj Ferfet <m.ferfet@ponowemu.pl>
2019-03-19 23:47:07 +0100

    Timetable module added

---
+++ WEBSYSTEM/Trimfit/Controllers/TimetableController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Trimfit.Controllers
+{
+    public class TimetableController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            ViewData["Header"] = "Zarządzaj grafikiem";
+            return View();
+        }
+    }
+}
