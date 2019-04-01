941f81d17e4a1376833e5350185746376c57c36d
Mikołaj Ferfet <m.ferfet@ponowemu.pl>
2019-04-01 21:53:16 +0200

    Update

---
+++ WEBSYSTEM/Trimfit/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Trimfit.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult List()
+        {
+            ViewData["Header"] = "Lista kategorii";
+            return View();
+        }
+    }
+}
---
+++ WEBSYSTEM/Trimfit/wwwroot/js/pages/customer.js
@@ -0,0 +1,45 @@
+﻿$("#add-new-customer").fireModal({
+    title: 'Dodaj nowego klienta',
+    body: $("#modal-new-customer"),
+    footerClass: 'bg-whitesmoke',
+    autoFocus: false,
+    onFormSubmit: function (modal, e, form) {
+
+        let form_data = $(e.target).serialize();
+        console.log(form_data);
+        // pobieramy aktualne wartości z formularza
+        var timetable_name = $("#timetable_name").val();
+        var timetable_status = $("#timetable_status").val();
+        // i przekazujemy ajaxem
+        $.ajax({
+            url: "/Customer/AddAsync",
+            method: "POST",
+            data: {
+                "timetable_name": timetable_name,
+                "timetable_status": timetable_status
+            },
+            success: function () {
+                modal.find('.modal-body').prepend('<div class="alert alert-success">Użytkownik został dodany</div>');
+                form.stopProgress();
+            },
+            error: function () {
+                modal.find('.modal-body').prepend('<div class="alert alert-error">Error occured. Contact with administrator!</div>')
+                form.stopProgress();
+            }
+        });
+
+        e.preventDefault();
+    },
+    shown: function (modal, form) {
+        console.log(form);
+    },
+    buttons: [
+        {
+            text: 'Dodaj',
+            submit: true,
+            class: 'btn btn-primary btn-shadow',
+            handler: function (modal) {
+            }
+        }
+    ]
+});
