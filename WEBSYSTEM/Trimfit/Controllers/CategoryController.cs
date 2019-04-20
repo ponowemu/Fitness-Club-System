720262726e08b229187ee45cf981ea63c4322806
Mikołaj Ferfet <m.ferfet@ponowemu.pl>
2019-04-20 13:23:03 +0200

    Categories controller modified

--- WEBSYSTEM/Trimfit/Controllers/CategoryController.cs
+++ WEBSYSTEM/Trimfit/Controllers/CategoryController.cs
@@ -1,21 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Trimfit.Data;
+using Trimfit.Models;
 
 namespace Trimfit.Controllers
 {
     public class CategoryController : Controller
     {
+        public async Task<List<Category>> GetAsync()
+        {
+            ApiContext _context = new ApiContext();
+            List<Category> categories = new List<Category>();
+
+            var categories_response = await _context.GetRequest("Categories/");
+            var categories_list = JsonConvert.DeserializeObject<List<Category>>(categories_response.Value.ToString());
+
+            foreach (var item in categories_list)
+            {
+                if (item.Category_Type == 1)
+                    categories.Add(item);
+            }
+
+            return categories;
+        }
+        public async Task<JsonResult> AddAsync(string category_name, string category_color)
+        {
+
+            ApiContext _context = new ApiContext();
+            Category category = new Category();
+            string result = "";
+
+            try
+            {
+                category.Category_Name = category_name;
+                category.Category_Color = category_color;
+                category.Category_Type = 1;
+
+                JsonResult response = await _context.PostRequest("Categories/", category);
+                result = response.Value.ToString();
+                // zastanowić się w jaki sposób czytać i przekazywać responsy!!!
+            }
+            catch (HttpRequestException e)
+            {
+                return new JsonResult(e.Message);
+            }
+
+            return new JsonResult("OK");
+        }
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult List()
+        public async Task<IActionResult> List()
         {
             ViewData["Header"] = "Lista kategorii";
-            return View();
+
+            var categories = await this.GetAsync();
+
+            return View(categories);
         }
     }
 }
---
+++ WEBSYSTEM/Trimfit/wwwroot/js/pages/category.js
@@ -0,0 +1,49 @@
+﻿"use strict";
+
+
+
+$("#add-new-category").fireModal({
+    title: 'Dodaj nową kategorię',
+    body: $("#new-category"),
+    footerClass: 'bg-whitesmoke',
+    autoFocus: false,
+    onFormSubmit: function (modal, e, form) {
+
+        let form_data = $(e.target).serialize(); // <- to może się przydać
+        console.log(form_data);
+        $.ajax({
+            url: "/Category/AddAsync",
+            method: "POST",
+            data: {
+                "category_name": $('#category_name').val(),
+                "category_color": $('#category_color').val()
+            },
+            success: function () {
+                form.stopProgress();
+                swal('Kategoria utworzona', 'Pomyślnie dodano nową kategorię!', 'success').then((value) => {
+                    location.reload();
+                });
+            },
+            error: function () {
+                form.stopProgress();
+                swal('Błąd', 'Nie udało się dodać kategorii. Skontaktuj się z administratorem!', 'error').then((value) => {
+                    location.reload();
+                });
+            }
+        });
+
+        e.preventDefault();
+    },
+    shown: function (modal, form) {
+        console.log(form);
+    },
+    buttons: [
+        {
+            text: 'Dodaj',
+            submit: true,
+            class: 'btn btn-primary btn-shadow',
+            handler: function (modal) {
+            }
+        }
+    ]
+});
