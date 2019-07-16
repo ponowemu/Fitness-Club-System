08ed64c98ac4418c49d9addd0aa0d829c026e421
Unknown <michal.mjk1@gmail.com>
2019-07-16 15:42:50 +0200

    Activity added
    Cardview implementation

--- MOBILE/TrimfitMobile/app/src/main/java/pl/smartica/trimfitmobile/ServiceHolder.kt
+++ MOBILE/TrimfitMobile/app/src/main/java/pl/smartica/trimfitmobile/ServiceHolder.kt
@@ -1,14 +1,10 @@
 package pl.smartica.trimfitmobile
 
-import android.content.Context
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.service_item.view.*
-import kotlinx.android.synthetic.main.service_item.service_header
 
 class ServiceHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     var headerText: TextView? = null
