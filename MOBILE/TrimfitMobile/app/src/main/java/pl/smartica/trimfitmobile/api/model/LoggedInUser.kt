b2c9f0a8a7d638f41d88f7ee73fbbdf75253a314
Michał Kamiński <michal.mjk1@gmail.com>
2019-08-24 21:44:36 +0200

    Android update
    -Complete folder remap
    -Fixed login

---
+++ MOBILE/TrimfitMobile/app/src/main/java/pl/smartica/trimfitmobile/api/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package pl.smartica.trimfitmobile.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
---
+++ MOBILE/TrimfitMobile/app/src/main/java/pl/smartica/trimfitmobile/api/model/User.kt
@@ -0,0 +1,4 @@
+package pl.smartica.trimfitmobile.api.model
+
+class User {
+}
